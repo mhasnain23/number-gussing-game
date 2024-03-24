814101a99286e75dd9c171c126d410203c821010
mhasnain23 <tryfrelancer23@gmail.com>
2024-03-24 04:31:00 -0700

    nit

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([{
+        type: "number",
+        name: "randomNumber",
+        message: "Enter your gussed number between 1 and 6",
+    }]);
+if (answer.randomNumber === randomNumber) {
+    console.log(chalk.green(("Congragulations, you gussed the correct number.")));
+}
+else {
+    console.log(chalk.red(("you failed, try again.")));
+}
